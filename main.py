5c08daa32e5636bccf18c91969d5060e4ed23d55
Dumbledore <91378876+Dumbledore295@users.noreply.github.com>
2021-10-20 14:27:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+rdeca = 18
+
+while True:
+    GPIO.setup(rdeca,GPIO.OUT)
+    print ("LED on")
+    GPIO.output(rdeca,GPIO.HIGH)
+
+    time.sleep(1)
+
+    print ("LED off")
+    GPIO.output(rdeca,GPIO.LOW)
+    time.sleep(1)
